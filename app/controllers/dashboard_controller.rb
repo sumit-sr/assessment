25350017863dca8051c8060e70c1f7b049c570bd
Developer <developer@gmail.com>
2018-04-06 15:47:59 +0530

    Added admin panel for users

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,22 @@
 class DashboardController < ApplicationController
+  before_action :redirect_as_per_user_role, only: [:home]
 
   def home;end
+
+  private
+
+  def redirect_as_per_user_role
+    if user_signed_in?
+      if current_user.has_role?(:admin)
+        redirect_to admin_users_path
+      elsif current_user.has_role?(:faculty)
+        redirect_to admin_users_path
+      elsif current_user.has_role?(:student)
+        redirect_to admin_users_path
+      end
+    else
+      return true
+    end
+  end
+
 end
