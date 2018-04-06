0b7c22959cb61f4143938deb28d8e3c0ad3f9eb2
Developer <developer@gmail.com>
2018-04-06 16:41:40 +0530

    Update association

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180406103542) do
+ActiveRecord::Schema.define(version: 20180406104023) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,15 @@ ActiveRecord::Schema.define(version: 20180406103542) do
 
   create_table "assignments", force: :cascade do |t|
     t.string "name"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.date "dob"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
